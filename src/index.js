234327723f4fff82e85ec33b8ab1748a74f2064d
AnkitVasita <vasitaankit97@gmail.com>
2021-09-25 12:08:53 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { GlobalProvider } from "./context/GlobalState";
+// import reducer, { initialState } from "./context/AppReducer";
+// import { GlobalProvider, initialState } from "./context/GlobalState";
+
+ReactDOM.render(
+  <GlobalProvider>
+    <App />
+  </GlobalProvider>,
+  document.getElementById("root")
+);
